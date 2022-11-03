17f50e7f045c6b6e3860e551b6ba2e03ff2c76f0
Arvind Gorur <arvind.gorur@hotmail.com>
2022-11-03 15:31:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Hello, World!')
+streamlit.title('My New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlist.text('Hard-Boiled Free-Range Egg')
