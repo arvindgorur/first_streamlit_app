57d66744f9dde5b3728bd7b255271cbb2b45b5aa
Arvind Gorur <arvind.gorur@hotmail.com>
2022-11-03 15:38:25 -0400

    Update streamlit_app.py
    Using pandas to create dataframe and display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My New Healthy Diner')
 
@@ -9,3 +10,6 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
 
 streamlit.header('Build Your Own Fruit Smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txe")
+streamlit.dataframe(my_fruit_list)
